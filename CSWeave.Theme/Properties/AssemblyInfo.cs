d4d9176d6938acf5012130e5c4a8d3df1d6c3c4f
johteto <johteto@LTR90FAYJ0>
2017-12-08 11:58:47 +0200

    Updated Nuget references.

--- CSWeave.Theme/Properties/AssemblyInfo.cs
+++ CSWeave.Theme/Properties/AssemblyInfo.cs
@@ -1,19 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle ("csweave - Literate Programming Tool for C#")]
 [assembly: AssemblyCopyright ("Copyright (c) 2017 Tommi Johtela")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("0e9474f8-0265-4eff-9522-a667d192a1fd")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,20 +1,10 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle ("csweave - Literate Programming Tool for C#")]
 [assembly: AssemblyCopyright ("Copyright (c) 2017 Tommi Johtela")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible (false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid ("a9536d25-0cca-436b-8cfe-6eb9e8ada75d")]
 
 // Version information for an assembly consists of the following four values:
 //
